6e4ad085af37dca692f6417f1708fa51f99d3b0d
Michael Koceja and Rose Sponseller <rose@>
2017-05-01 13:04:24 -0700

    make code dry-er

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@ $(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var flavors = ["iceCream1", "iceCream2", "iceCream3"];
-    var favoriteArray [iceCream1, iceCream2, iceCream3];
 
     flavors.forEach(function(flavor) {
-      var userInput = $("input#" + flavor).val();
-      $("." + flavor).text(userInput);
-      $("ul").append("<li>" + userInput + "</li>");
+      var flavor = $("input#" + flavor).val();
+      $("." + flavor).text(flavor);
+      $("ul").append("<li>" + flavor + "</li>");
     });
   });
 });
